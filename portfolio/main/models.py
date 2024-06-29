d47640847ada0856a4bc48bbfcd980ca93c4f8fd
Olutayo0910 <olutayoogunlade0910@gmail.com>
2024-06-29 06:45:39 +0100

    model set up

--- portfolio/main/models.py
+++ portfolio/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name="projects")
+    link = models.URLField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.title
